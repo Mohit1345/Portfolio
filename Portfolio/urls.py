2e50535d64c43b776f65d53993a1c5bb09bfdb23
Mohit Chawla <98903945+Mohit1345@users.noreply.github.com>
2023-06-27 16:55:11 +0530

    Update urls.py

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("mohit.urls")),
-    path('blog_effect',include('blog_effect.urls')),
+    path('blog_effect/',include('blog_effect.urls')),
 ]
