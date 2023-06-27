96d3ed01a928740f9229c23fb2ee72a9f133204a
Mohit Chawla <98903945+Mohit1345@users.noreply.github.com>
2023-06-27 16:34:25 +0530

    Update urls.py
    added blog_effect

--- blog_effect/urls.py
+++ blog_effect/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from blog_effect import views
 
 urlpatterns = [
-    path('', views.blog_index,name = "blog_index"),
-    path('<str:title>/', views.blog_description, name='blog_description'),
+    path('blog_effect/', views.blog_index,name = "blog_index"),
+    path('blog_effect/<str:title>/', views.blog_description, name='blog_description'),
 ]
