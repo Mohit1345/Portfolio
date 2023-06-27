f0ec029c9593dc7781d2646f5c646b163602cced
Mohit Chawla <98903945+Mohit1345@users.noreply.github.com>
2023-06-27 13:25:08 +0530

    Update settings.py
    commented all dbs

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'neondb',
-    'USER': 'chawlamohit45',
-    'PASSWORD': DB_PASS,
-    'HOST': 'ep-weathered-salad-387081.ap-southeast-1.aws.neon.tech',
-    'PORT': '5432',
-  }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'neondb',
+#     'USER': 'chawlamohit45',
+#     'PASSWORD': DB_PASS,
+#     'HOST': 'ep-weathered-salad-387081.ap-southeast-1.aws.neon.tech',
+#     'PORT': '5432',
+#   }
+# }
 
 
 # Password validation
