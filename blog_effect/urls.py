e535fe457179e0057dac5e86e6dc5218d3fc9ce7
Mohit Chawla <chawlamohit45@gmail.com>
2023-06-28 13:32:22 +0530

    added responsiveness

--- blog_effect/urls.py
+++ blog_effect/urls.py
@@ -4,5 +4,5 @@ from blog_effect import views
 
 urlpatterns = [
     path('', views.blog_index,name = "blog_index"),
-    path('<str:title>/', views.blog_description, name='blog_description'),
+    path('<str:title>', views.blog_description, name='blog_description'),
 ]
